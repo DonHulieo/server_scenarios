9a25d3abf74d53725bcdb6d33743e1bf322a8633
kibook <kibuzhj@gmail.com>
2021-11-29 17:22:54 -0500

    Add sp_manifest.ymt to manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,3 +4,6 @@ game "gta5"
 author "TayMcKenzieNZ"
 description "Custom and modified scenarios for FiveM"
 url "https://github.com/TayMcKenzieNZ/server-scenarios"
+
+file "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_FILE" "sp_manifest.ymt"
