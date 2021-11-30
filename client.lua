f20b3b40bb32bb7a94dec6b79deeab5dc3756799
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-11-30 14:03:17 +1300

    Added client.lua

---
+++ client.lua
@@ -0,0 +1,18 @@
+-- This is used to calm down the AI animals at the Madrazo Ranch --
+
+
+SetRelationshipBetweenGroups(1, `HEN`, `PLAYER`)
+SetRelationshipBetweenGroups(1, `DEER`, `PLAYER`)
+SetRelationshipBetweenGroups(1, `WILD_ANIMAL`, `PLAYER`)
+SetRelationshipBetweenGroups(1,`DOMESTIC_ANIMAL`, `PLAYER`)
+
+
+
+
+
+
+
+
+
+
+
