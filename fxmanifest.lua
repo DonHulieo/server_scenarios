989c374938f758978a9712bfaabf3b9b2621036a
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-11-30 14:02:00 +1300

    Updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,3 +7,14 @@ url "https://github.com/TayMcKenzieNZ/server-scenarios"
 
 file "sp_manifest.ymt"
 data_file "SCENARIO_POINTS_OVERRIDE_FILE" "sp_manifest.ymt"
+
+
+files {
+	'events.meta',
+	'relationships.dat'
+}
+
+data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
+
+server_script 'server.lua'
+client_script 'client.lua'
