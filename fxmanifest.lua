b2acf367ac12972af693005bb8b2bdd7207e516c
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-04-14 11:18:02 +1200

    Fix For PSO & Removed Unnecessary Files
    Fixed sp_manifest into pso format, removed unnecessary files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,8 @@
 fx_version "cerulean"
 game "gta5"
 
-author "TayMcKenzieNZ"
-description "Custom and modified scenarios for FiveM"
-url "https://github.com/TayMcKenzieNZ/server-scenarios"
-
 file "sp_manifest.ymt"
-data_file "SCENARIO_POINTS_OVERRIDE_FILE" "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
 
 
 files {
@@ -16,5 +12,7 @@ files {
 
 data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
 
+
 server_script 'server.lua'
 client_script 'client.lua'
+
