edc468bfa51417d2b90b81dc1fd70553764f0bd8
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-04-17 03:50:13 +1200

    Moved server_scenarios into separate folder

---
+++ server_scenarios/client.lua
@@ -0,0 +1,18 @@
+
+SetRelationshipBetweenGroups(1, GetHashKey("HEN"), GetHashKey('PLAYER'))
+SetRelationshipBetweenGroups(1, GetHashKey("DEER"), GetHashKey('PLAYER'))
+SetRelationshipBetweenGroups(1, GetHashKey("WILD_ANIMAL"), GetHashKey('PLAYER'))
+SetRelationshipBetweenGroups(1, GetHashKey("CAT"), GetHashKey('PLAYER'))
+SetRelationshipBetweenGroups(1, GetHashKey("DOMESTIC_ANIMAL"), GetHashKey('PLAYER'))
+SetRelationshipBetweenGroups(1, GetHashKey("GUARD_DOG"), GetHashKey('PLAYER'))
+
+
+
+
+
+
+
+
+
+
+
